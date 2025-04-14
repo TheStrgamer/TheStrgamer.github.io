db433308d2443d268341e8492e57cb149293fb05
Konrad <konradseime@gmail.com>
2025-04-15 00:17:08 +0200

    feat: Made simple image slider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import ImageSlider from './components/ImageSlider.jsx'
 
 function App() {
   return (
     <>
-
-
+      <ImageSlider images={["Baines.png", "dante.jpg", "vergil.png"]} />
     </>
   )
 }
---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import '../assets/ImageSlider.css'
+
+function ImageSlider({images}) {
+  
+
+  const [index, setIndex] = useState(0);
+  const activeImage = images[index]
+  
+  function updateIndex(i) {
+    let newIndex = index;
+    newIndex += i;
+    if (newIndex < 0) {
+      newIndex = images.length + newIndex;
+    } if (newIndex >= images.length) {
+      newIndex = newIndex - images.length;
+      
+    }
+    setIndex(newIndex);
+  }
+  function nextImage() {
+    updateIndex(1);
+  }
+  function prevImage() {
+    updateIndex(-1);
+  }
+  return (
+    <>
+    <div className="imageSlide">
+      <button className="leftButton" onClick={nextImage}>←</button>
+      <button className="rightButton" onClick={prevImage}>→</button>
+      <img src={activeImage} alt={`Slide ${index}`} />
+    </div>
+
+    </>
+  )
+}
+
+export default ImageSlider

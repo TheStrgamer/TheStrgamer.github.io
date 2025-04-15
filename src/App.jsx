9cdf5dc3e88610e6757ef64f1e813310cb6328fe
Konrad <konradseime@gmail.com>
2025-04-16 01:22:27 +0200

    style: improved navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import ImageSlider from './components/ImageSlider.jsx'
-import NavBar from './components/NavBar.jsx'
 
 function App() {
   return (
-    <>
-      <NavBar/>
+    <div className='main'>
       <ImageSlider images={["Baines.png", "dante.jpg", "vergil.png"]} />
-    </>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,14 @@ import NavBarButton from './NavBarButton';
 
 function NavBar() {
   
-  const pages = [["Home", "/"],["Experience", "/experience"],["Projects", "/projects"]]
+  const pages = [["Home", "/", "#be1030"],["Experience", "/experience", "#30ae00"],["Projects", "/projects", "#0040ae"]]
   const [index, setIndex] = useState(0);
   const activePage = pages[index]
 
   let buttons = [];
   for (var i = 0; i<pages.length; i++) {
     buttons.push(
-        <NavBarButton name={pages[i][0]} route={pages[i][1]} />
+        <NavBarButton name={pages[i][0]} route={pages[i][1]} color={pages[i][2]} />
     )
   }
 

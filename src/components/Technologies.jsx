a38a665278e5077db3c3b7ca2874a5608de7324e
Konrad <konradseime@gmail.com>
2025-04-17 16:44:19 +0200

    feat: added technologies

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,21 @@
+import "../assets/Technologies.css"
+function Technologies() {
+
+    const images = ["cpp.png", "django.png", "Vue.png", "java.webp", "js.webp", "python.webp", "unity.png","typescript.png","cs.png"]
+    const titles = ["C++","Django","Vue","Java","Javascript","Python","Unity","Typescript","C#"]
+
+    const logos = []
+    for (let i = 0; i < images.length; i++) {
+        logos.push(<img src={images[i]} className="logo" title={titles[i]} />)
+    }
+
+    return (
+        <>
+        <h2>Technologies</h2>
+        <div className="technologies">
+            {logos}
+        </div>
+        </>
+    )
+}
+export default Technologies
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import ImageSlider from "../components/ImageSlider"
+import Technologies from "../components/Technologies"
 function Home() {
     return(
         <>
         <h2>Konrad Øye Seime</h2>
-        <div class="content">
+        <div className="content">
           <ImageSlider images={["me.jpg", "hold.jpg", "jump.jpg", "climb.jpg"]} />
           <div className="text">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
           </div>
+          <Technologies/>
         </div>
         </>
     )

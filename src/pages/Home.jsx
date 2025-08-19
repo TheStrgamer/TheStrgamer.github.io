04107f1fed97b826ee67aae8c0f554c3ea1504cd
Konrad <konradseime@gmail.com>
2025-08-19 14:39:30 +0200

    updated images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
         <>
         <h1>Konrad Øye Seime</h1>
         <div className="content">
-          <ImageSlider images={["me.jpg", "nm1.jpg", "tur.jpg", "hold.jpg", "hand.jpg", "nm2.jpg"]} />
+          <ImageSlider images={["me.jpg", "nm1.jpg", "tur.jpg", "hold.jpg", "cogi2.png", "hand.jpg", "nm2.jpg", "cogi1.JPG"]} />
           <div className="text">
             <p>Mitt navn er Konrad Øye Seime, og jeg studerer <a href="https://www.ntnu.no/studier/bidata">Dataingeniør bachelor</a> ved NTNU Trondheim.
               På fritiden liker jeg å jobbe med ulike prosjekter, og delta i ulike studentverv. Jeg er bland annet medlem i Cogito NTNU, styremedlem i Make NTNU og er med i NTNUI Calisthenics. Jeg deltok i nm i streetworkout i 2025, og fikk 6. plass i strength and endurance.
